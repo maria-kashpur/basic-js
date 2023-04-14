a2fce111bc9493fc7d0946bca1e9ba0141d0c990
maria-kashpur <kashpur.maria@icloud.com>
2023-04-14 11:16:27 +0300

    feat: vigenere-cipher.js solved

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,94 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (type) {
+    this.type = type === true || type === undefined ? true : false;
+    this.alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+    this.dictionary = this.alfabet.reduce((accum, el, index) => {
+    accum[el] = index;
+    return accum;
+    }, {})
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error ('Incorrect arguments!')
+    }
+
+    console.log(this.type)
+    message = message.toUpperCase().split('')
+    key = key.toUpperCase()
+    let onlyWords = message.reduce((acc, el) => {
+     if (this.dictionary[el] !== undefined) {
+        acc.push(el)
+     }
+     return acc
+    }, [])
+
+    onlyWords = onlyWords.reduce((result, el, index) => {
+        let dictionaryValueMessege = this.dictionary[el]
+        let dictionaryValueKey = this.dictionary[key[index % key.length]]
+        let newIndex = (dictionaryValueMessege + dictionaryValueKey) % this.alfabet.length
+        result.push(this.alfabet[newIndex])
+        return result;
+     }, [])
+     
+     let count = 0;
+     let result =  message.reduce((accum, el) => {
+        if (this.dictionary[el] !== undefined) {
+           accum += onlyWords[count]
+           count += 1
+        } else {
+           accum += el
+        }
+        return accum
+     }, "")
+
+     if (this.type) {
+        return result
+     } else {
+        return result.split('').reverse().join('')
+     }
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error ('Incorrect arguments!')
+    }
+    encryptedMessage = encryptedMessage.split('')
+    key = key.toUpperCase()
+    let onlyWords = encryptedMessage.reduce((acc, el) => {
+     if (this.dictionary[el] !== undefined) {
+        acc.push(el)
+     }
+     return acc
+    }, [])
+
+    onlyWords = onlyWords.reduce((result, el, index) => {
+     let dictionaryValueMessege = this.dictionary[el]
+     let dictionaryValueKey = this.dictionary[key[index % key.length]]
+     let newIndex = (dictionaryValueMessege - dictionaryValueKey + this.alfabet.length) % this.alfabet.length
+     result.push(this.alfabet[newIndex])
+     return result;
+  }, [])
+
+
+    let count = 0;
+    let result = encryptedMessage.reduce((accum, el) => {
+       if (this.dictionary[el] !== undefined) {
+          accum += onlyWords[count]
+          count += 1
+       } else {
+          accum += el
+       }
+       return accum
+    }, "")
+
+    if (this.type) {
+        return result
+     } else {
+        return result.split('').reverse().join('')
+     }
   }
 }
 
