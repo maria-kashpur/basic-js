e4a33da3e4c6be0ec78b5e18c99b6b217ecf14c0
maria-kashpur <kashpur.maria@icloud.com>
2023-04-14 17:34:51 +0300

    feat: dns-stats.js solved

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  return domains.reduce((result, el) => {
+    let domain = el.split('.').reverse()
+
+     domain.reduce((accum, item) => {
+        accum += `.${item}`;
+        if (!result[accum]) {
+           result[accum] = 1;
+        } else {
+           result[accum] += 1
+        }
+        return accum;
+     }, '')
+
+     return result
+     
+  }, {})
 }
 
 module.exports = {
